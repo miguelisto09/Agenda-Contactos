3cd917c45332d4c2ed5606d271d8d3ea622e6c8d
Marianny19 <145990122+Marianny19@users.noreply.github.com>
2024-10-04 16:53:35 -0400

    agrego tipo de contacto

--- Agenda Contactos/MainWindow.xaml.cs
+++ Agenda Contactos/MainWindow.xaml.cs
@@ -20,5 +20,10 @@ namespace Agenda_Contactos
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
